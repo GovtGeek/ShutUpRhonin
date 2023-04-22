77032bce1e1dbb77fabb36f3746007df6b0f974f
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2023-04-21 22:05:03 -0700

    Update ShutUpRhonin.lua
    Put the RIGHT sound files in.

--- ShutUpRhonin.lua
+++ ShutUpRhonin.lua
@@ -1,6 +1,6 @@
 local function OnEvent(self, event, isLogin, isReload)
 	if isLogin or isReload then
-        local RhoninEvent = { 559130, 559131, 559132, 559133, 559134, 559135, 559136, 559137, 559138 }
+        local RhoninEvent = { 559126, 559127, 559128, 559129, 559130, 559131, 559132, 559133, }
         for _, yell in pairs(RhoninEvent) do
             MuteSoundFile(yell)
         end
