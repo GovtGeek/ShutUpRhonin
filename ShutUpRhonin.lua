e28e5c24e161c2a8b28043483634200eb6a0e2dd
Road-block <dridzt.addons@hotmail.com>
2023-05-04 21:00:50 +0300

    filter the yell as well in a locale-agnostic way

--- ShutUpRhonin.lua
+++ ShutUpRhonin.lua
@@ -1,9 +1,22 @@
+local function RhoninFilter(self, event, msg, author, ...)
+    local lang, channel, author2, flags, zonechannel, channelid, channelbase, langid, lineid, guid = ...
+    local zone = C_Map.GetBestMapForUnit("player")
+    local _, _, _, _, _, npcid = strsplit("-", guid or "")
+    npcid = tonumber(npcid)
+    if npcid == 16128 and zone == 125 then -- language independent check
+        return true -- filter it
+    else
+        return false, msg, author, ... -- let it pass
+    end
+end
+
 local function OnEvent(self, event, isLogin, isReload)
 	if isLogin or isReload then
         local RhoninEvent = { 559126, 559127, 559128, 559129, 559130, 559131, 559132, 559133, }
         for _, yell in pairs(RhoninEvent) do
             MuteSoundFile(yell)
         end
+        ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", RhoninFilter)
 	end
 end
 
